88c290626e35e39ae1c63f090a5b4fa68a7c984e
SanaTanwir <156840355+SanaTanwir@users.noreply.github.com>
2024-10-03 16:10:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//  in js camelCasing is important
+
 
 let count = 0 
 
@@ -13,10 +13,10 @@ function increment () {
 
 function save() {
     let saved = "  " + count + " - "
-    // can use also .textContent instead of innerText to get spaces 
+    
     saveEl.innerText += saved  
     console.log (count)
-    //  to set back the counter to 0 after saving set countEl to 0  and to start from 0 again set count to 0 as well
+    
     countEl.innerText = 0
     count = 0 
 }
